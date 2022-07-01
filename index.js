5f87516a10cb0ab5f23d42b2212253be21c9ada8
Thales Alves <thalesalves758@gmail.com>
2022-07-01 13:06:11 -0300

    Use auth routes

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import authRoutes from './src/routes/authRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/', authRoutes);
 
 const PORT = process.env.PORT || 5000; /* eslint-disable-line */
 app.listen(PORT, () => {
