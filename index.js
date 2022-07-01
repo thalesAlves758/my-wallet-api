d0a73e7fd33c951f9f6814045191f785f79491b7
Thales Alves <thalesalves758@gmail.com>
2022-07-01 13:06:51 -0300

    Remove route from app.use - line 13

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/', authRoutes);
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 5000; /* eslint-disable-line */
 app.listen(PORT, () => {
