91f946a60ab5a9e48845b573613429048e0d5d51
Thales Alves <thalesalves758@gmail.com>
2022-07-02 14:02:35 -0300

    Add POST /sign-out route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import authController from '../controllers/authController.js';
+import validateUser from '../middlewares/validateUser.js';
 
 const router = express.Router();
 
 router.post('/sign-up', authController.signUp);
 router.post('/sign-in', authController.signIn);
+router.post('/sign-out', validateUser, authController.signOut);
 
 export default router;
