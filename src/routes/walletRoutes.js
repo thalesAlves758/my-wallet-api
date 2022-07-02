2af9fcb5e7bf672e152296a4696ef6bd38576ab0
Thales Alves <thalesalves758@gmail.com>
2022-07-02 15:21:18 -0300

    Refactor to POST /wallet/new-record

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -6,6 +6,6 @@ import validateUser from '../middlewares/validateUser.js';
 const router = express.Router();
 
 router.get('/wallet', validateUser, walletController.getWallet);
-router.post('/wallet/new-input', validateUser, walletController.createInput);
+router.post('/wallet/new-record', validateUser, walletController.createRecord);
 
 export default router;
