91833bafea2d57535ef11fc8cac78f7252504db0
Thales Alves <thalesalves758@gmail.com>
2022-07-02 00:48:34 -0300

    Import and use wallet routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import authRoutes from './src/routes/authRoutes.js';
+import walletRoutes from './src/routes/walletRoutes.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(authRoutes);
+app.use(walletRoutes);
 
 const PORT = process.env.PORT || 5000; /* eslint-disable-line */
 app.listen(PORT, () => {
