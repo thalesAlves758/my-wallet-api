a59f246e6cc75a1964776f0a16b52920e8e920f6
Thales Alves <thalesalves758@gmail.com>
2022-07-02 15:00:29 -0300

    Create createInput handler

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -1,23 +1,34 @@
+import Joi from 'joi';
+
 import db from '../database/mongo.js';
 import httpStatus from '../utils/httpStatus.js';
+
+async function getWalletByUserId(userId) {
+  return db.collection('wallets').findOne({ user_id: userId });
+}
+
+async function getCashFlowByWalletId(walletId) {
+  const DESC = -1;
+
+  return db
+    .collection('cashFlows')
+    .find({ wallet_id: walletId })
+    .sort({ _id: DESC })
+    .toArray();
+}
 
 async function getWallet(req, res) {
   const { user } = res.locals;
 
   try {
-    const wallet = await db
-      .collection('wallets')
-      .findOne({ user_id: user._id });
+    const wallet = await getWalletByUserId(user._id);
 
     if (!wallet) {
       res.sendStatus(httpStatus.NOT_FOUND);
       return;
     }
 
-    const cashFlow = await db
-      .collection('cashFlows')
-      .find({ wallet_id: wallet._id })
-      .toArray();
+    const cashFlow = await getCashFlowByWalletId(wallet._id);
 
     res.send({ balance: wallet.balance, cashFlow });
   } catch (error) {
@@ -25,4 +36,61 @@ async function getWallet(req, res) {
   }
 }
 
-export default { getWallet };
+async function createInput(req, res) {
+  const ZERO = 0;
+  const DECIMALS_PLACES = 2;
+
+  const { user } = res.locals;
+
+  const { value, description } = req.body;
+
+  const schema = Joi.object({
+    value: Joi.number().precision(DECIMALS_PLACES).greater(ZERO).required(),
+    description: Joi.string().required(),
+  });
+
+  const validation = schema.validate(
+    { value, description },
+    { abortEarly: false, convert: false }
+  );
+
+  if (validation.error) {
+    const error = {};
+
+    validation.error.details.forEach(({ path, message }) => {
+      error[path] = message;
+    });
+
+    res.status(httpStatus.UNPROCESSABLE_ENTITY).send({ error });
+    return;
+  }
+
+  try {
+    const wallet = await getWalletByUserId(user._id);
+
+    if (!wallet) {
+      res.sendStatus(httpStatus.NOT_FOUND);
+      return;
+    }
+
+    await db.collection('cashFlows').insertOne({
+      type: 'input',
+      description,
+      value,
+      date: Date.now(),
+      wallet_id: wallet._id,
+    });
+
+    await db
+      .collection('wallets')
+      .updateOne({ _id: wallet._id }, { $inc: { balance: value } });
+
+    const cashFlow = await getCashFlowByWalletId(wallet._id);
+
+    res.send({ balance: wallet.balance + value, cashFlow });
+  } catch (error) {
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export default { getWallet, createInput };
