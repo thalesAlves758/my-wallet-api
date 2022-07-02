1359eef1592bb292325dead37fc4ffd48cca1330
Thales Alves <thalesalves758@gmail.com>
2022-07-02 15:00:47 -0300

    Create POST /wallet/new-input route

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -6,5 +6,6 @@ import validateUser from '../middlewares/validateUser.js';
 const router = express.Router();
 
 router.get('/wallet', validateUser, walletController.getWallet);
+router.post('/wallet/new-input', validateUser, walletController.createInput);
 
 export default router;
