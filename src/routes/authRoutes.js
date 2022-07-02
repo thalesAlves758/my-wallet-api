bd54bf7d5aef0a99a4371ecdf07b80c97c66f4f8
Thales Alves <thalesalves758@gmail.com>
2022-07-01 23:08:13 -0300

    Add POST /sign-in route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ import authController from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/sign-up', authController.signUp);
+router.post('/sign-in', authController.signIn);
 
 export default router;
