71a59d1cc1a0318a64750de9c0b299806f6247cf
Thales Alves <thalesalves758@gmail.com>
2022-07-02 14:02:18 -0300

    Create signOut handler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -103,4 +103,24 @@ async function signIn(req, res) {
   res.send({ token: thisSession.token, email: user.email, name: user.name });
 }
 
-export default { signUp, signIn };
+async function signOut(req, res) {
+  const { session: currentSession } = res.locals;
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+
+  if (currentSession.token !== token) {
+    res.sendStatus(httpStatus.UNAUTHORIZED);
+    return;
+  }
+
+  const session = await db.collection('sessions').findOneAndDelete({ token });
+
+  if (!session) {
+    res.sendStatus(httpStatus.NOT_FOUND);
+    return;
+  }
+
+  res.sendStatus(httpStatus.NO_CONTENT);
+}
+
+export default { signUp, signIn, signOut };
