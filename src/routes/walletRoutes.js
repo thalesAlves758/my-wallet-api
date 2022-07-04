47bceb56acd4711e67de28b4821818634acb95c1
Thales Alves <thalesalves758@gmail.com>
2022-07-04 09:28:16 -0300

    Remove /wallet/new-record route

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -6,6 +6,5 @@ import validateUser from '../middlewares/validateUser.js';
 const router = express.Router();
 
 router.get('/wallet', validateUser, walletController.getWallet);
-router.post('/wallet/new-record', validateUser, walletController.createRecord);
 
 export default router;
