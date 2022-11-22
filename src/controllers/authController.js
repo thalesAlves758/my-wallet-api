2faa158c116aa927c78b3fb604a2ddfe5f7b903e
Thales Alves <thalesalves758@gmail.com>
2022-11-22 19:10:01 -0300

    feat: refactor signUp function with services, repositories and middlewares

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,62 +1,17 @@
 import Joi from 'joi';
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+
 import sanitizeStrings from '../utils/sanitizeStrings.js';
 
 import httpStatus from '../utils/httpStatus.js';
 import db from '../database/mongo.js';
+import { signUpUser } from '../services/authService.js';
 
 async function signUp(req, res) {
-  const [name, email, password, confirmPassword] = sanitizeStrings([
-    req.body.name || '',
-    req.body.email || '',
-    req.body.password || '',
-    req.body.confirmPassword || '',
-  ]);
+  const { name, email, password } = req.body;
 
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    confirmPassword: Joi.string().required().valid(Joi.ref('password')),
-  });
-
-  const validation = schema.validate(
-    { name, email, password, confirmPassword },
-    { abortEarly: false }
-  );
-
-  if (validation.error) {
-    const error = {};
-
-    validation.error.details.forEach(({ path, message }) => {
-      error[path] = message;
-    });
-
-    res.status(httpStatus.UNPROCESSABLE_ENTITY).send({ error });
-    return;
-  }
-
-  const user = await db.collection('users').findOne({ email });
-
-  if (user) {
-    res.sendStatus(httpStatus.CONFLICT);
-    return;
-  }
-
-  const saltRounds = 10;
-  const hashedPassword = bcrypt.hashSync(password, saltRounds);
-
-  const newUser = await db.collection('users').insertOne({
-    name,
-    email,
-    password: hashedPassword,
-  });
-
-  await db.collection('wallets').insertOne({
-    user_id: newUser.insertedId,
-    balance: 0,
-  });
+  await signUpUser({ name, email, password });
 
   res.sendStatus(httpStatus.CREATED);
 }
