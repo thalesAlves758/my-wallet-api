ea692a6b3ea08cef1d5abf7c0098658350c2c909
Thales Alves <thalesalves758@gmail.com>
2022-11-22 19:11:01 -0300

    feat: use express-async-errors and user index router

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import authRoutes from './src/routes/authRoutes.js';
-import walletRoutes from './src/routes/walletRoutes.js';
-import recordRoutes from './src/routes/recordRoutes.js';
+import errorHandler from './src/middlewares/errorHandler.js';
+import router from './src/routes/index.js';
 
 dotenv.config();
 
@@ -12,11 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(authRoutes);
-app.use(walletRoutes);
-app.use(recordRoutes);
+app.use('/', router);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000; /* eslint-disable-line */
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
