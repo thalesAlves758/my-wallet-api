d5e227bba615d34606f37fd814fbf2cba5b4c3c3
Thales Alves <thalesalves758@gmail.com>
2022-11-23 10:35:21 -0300

    feat: create compareTexts function

--- src/utils/bcryptUtils.js
+++ src/utils/bcryptUtils.js
@@ -1,9 +1,13 @@
 import bcrypt from 'bcrypt';
 
-function encryptText(text) {
+export function encryptText(text) {
   const saltRounds = 10;
 
   return bcrypt.hashSync(text, saltRounds);
+}
+
+export function compareTexts(encryptedText, text) {
+  return bcrypt.compareSync(text, encryptedText);
 }
 
 export default encryptText;
