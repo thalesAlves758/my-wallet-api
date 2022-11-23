f68d9d48a7d44c0f704ed58aab1c747837a4f691
Thales Alves <thalesalves758@gmail.com>
2022-11-23 10:36:10 -0300

    refactor: refactor signIn request handler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,6 @@
-import Joi from 'joi';
-import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
-
 import httpStatus from '../utils/httpStatus.js';
 import db from '../database/mongo.js';
-import { signUpUser } from '../services/authService.js';
+import { signInUser, signUpUser } from '../services/authService.js';
 
 async function signUp(req, res) {
   const { name, email, password } = req.body;
@@ -17,49 +13,9 @@ async function signUp(req, res) {
 async function signIn(req, res) {
   const { email, password } = req.body;
 
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  });
+  const result = await signInUser({ email, password });
 
-  const validation = schema.validate(
-    {
-      email,
-      password,
-    },
-    { abortEarly: false }
-  );
-
-  if (validation.error) {
-    const error = {};
-
-    validation.error.details.forEach(({ path, message }) => {
-      error[path] = message;
-    });
-
-    res.status(httpStatus.UNPROCESSABLE_ENTITY).send({ error });
-    return;
-  }
-
-  const user = await db.collection('users').findOne({ email });
-
-  if (!user || !bcrypt.compareSync(password, user.password)) {
-    res
-      .status(httpStatus.UNAUTHORIZED)
-      .send({ error: 'Incorrect email or password' });
-
-    return;
-  }
-
-  const thisSession = {
-    token: uuid(),
-    user_id: user._id,
-  };
-
-  await db.collection('sessions').insertOne(thisSession);
-
-  res.locals.session = thisSession.token;
-  res.send({ token: thisSession.token, email: user.email, name: user.name });
+  res.send(result);
 }
 
 async function signOut(req, res) {
