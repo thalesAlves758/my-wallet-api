fd2371cd0bd7f8edb7d62143a32a0ca4d832f7b4
Thales Alves <thalesalves758@gmail.com>
2022-11-23 10:35:40 -0300

    feat: create signInUser function

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
-import { conflictError } from '../errors/httpErrors.js';
+import { conflictError, unauthorizedError } from '../errors/httpErrors.js';
 import { create, findByEmail } from '../repositories/userRepository.js';
-import encryptText from '../utils/bcryptUtils.js';
+import { encryptText, compareTexts } from '../utils/bcryptUtils.js';
+import generateToken from '../utils/jwtUtils.js';
 
 export async function signUpUser({ name, email, password }) {
   await validateUserNotExists(email);
@@ -15,5 +16,37 @@ export async function validateUserNotExists(email) {
 
   if (user) {
     throw conflictError('An user with this email already exists');
+  }
+}
+
+export async function signInUser({ email, password }) {
+  const user = await validateUserExists(email);
+  validateUserPassword(user.password, password);
+
+  const token = generateToken({ userId: user._id });
+
+  delete user.password;
+
+  return {
+    ...user,
+    token,
+  };
+}
+
+export async function validateUserExists(email) {
+  const user = await findByEmail(email);
+
+  if (!user) {
+    throw unauthorizedError('Incorrect email or password');
+  }
+
+  return user;
+}
+
+export function validateUserPassword(encryptedPassword, bodyPassword) {
+  const result = compareTexts(encryptedPassword, bodyPassword);
+
+  if (!result) {
+    throw unauthorizedError('Incorrect email or password');
   }
 }
