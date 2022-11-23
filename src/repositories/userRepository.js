538f097b231bc7a9164641773e068405d0a72332
Thales Alves <thalesalves758@gmail.com>
2022-11-23 16:22:52 -0300

    feat: create a function to find a user by id

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -4,6 +4,10 @@ export async function findByEmail(email) {
   return db.collection('users').findOne({ email });
 }
 
+export async function findById(userId) {
+  return db.collection('users').findOne({ _id: userId });
+}
+
 export async function create({ email, name, password }) {
   return db.collection('users').insertOne({
     name,
