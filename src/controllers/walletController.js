e00276ae1da5cb16ed52b27f60461b9ac747675f
Thales Alves <thalesalves758@gmail.com>
2022-11-23 16:35:48 -0300

    refactor: refactor wallet controller

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -1,31 +1,7 @@
-import db from '../database/mongo.js';
-import httpStatus from '../utils/httpStatus.js';
-
 async function getWallet(req, res) {
-  const DESC = -1;
-
   const { user } = res.locals;
 
-  try {
-    const wallet = await db
-      .collection('wallets')
-      .findOne({ user_id: user._id });
-
-    if (!wallet) {
-      res.sendStatus(httpStatus.NOT_FOUND);
-      return;
-    }
-
-    const cashFlow = await db
-      .collection('cashFlows')
-      .find({ wallet_id: wallet._id })
-      .sort({ _id: DESC })
-      .toArray();
-
-    res.send({ balance: wallet.balance, cashFlow });
-  } catch (error) {
-    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-  }
+  res.send(user.wallet);
 }
 
-export default { getWallet };
+export default getWallet;
