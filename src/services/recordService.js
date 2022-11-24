1498c9234441f96add50405f734ec3aea4efab82
Thales Alves <thalesalves758@gmail.com>
2022-11-24 09:26:13 -0300

    feat: create record service

---
+++ src/services/recordService.js
@@ -0,0 +1,27 @@
+import create from '../repositories/recordRepository.js';
+import {
+  findById,
+  updateWalletBalance,
+} from '../repositories/userRepository.js';
+
+async function createRecord({ value, description, type, userId }) {
+  await create({ value, description, type, userId });
+
+  const valueToSum = type === 'input' ? value : toNegative(value);
+
+  await updateWalletBalance(userId, valueToSum);
+
+  const user = await findById(userId);
+
+  return {
+    updatedBalance: user.wallet.balance,
+  };
+}
+
+function toNegative(value) {
+  const MINUS_ONE = -1;
+
+  return MINUS_ONE * Math.abs(value);
+}
+
+export default createRecord;
