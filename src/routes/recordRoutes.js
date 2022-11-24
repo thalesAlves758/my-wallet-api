da66c67b048929af2a8afcf26d43448e61c06c77
Thales Alves <thalesalves758@gmail.com>
2022-11-24 09:25:00 -0300

    refactor: refactor POST /records route and use validateToken middleware

--- src/routes/recordRoutes.js
+++ src/routes/recordRoutes.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import recordController from '../controllers/recordController.js';
-import validateUser from '../middlewares/validateUser.js';
+import {
+  createRecord,
+  deleteRecord,
+  updateRecord,
+} from '../controllers/recordController.js';
+import validateToken from '../middlewares/validateToken.js';
+import validateSchema from '../middlewares/validateSchema.js';
+import recordSchema from '../schemas/recordSchema.js';
 
 const router = express.Router();
 
-router.post('/records', validateUser, recordController.createRecord);
-router.delete(
-  '/records/:recordId',
-  validateUser,
-  recordController.deleteRecord
-);
-router.put('/records/:recordId', validateUser, recordController.updateRecord);
+router.post('/', validateToken, validateSchema(recordSchema), createRecord);
+router.delete('/records/:recordId', validateToken, deleteRecord);
+router.put('/records/:recordId', validateToken, updateRecord);
 
 export default router;
