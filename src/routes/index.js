540cd2e77cec1c7dea171c61d5ae8a1c47a7b403
Thales Alves <thalesalves758@gmail.com>
2022-11-25 16:05:42 -0300

    fix: remove the use of wallet routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 
 import authRoutes from './authRoutes.js';
-import walletRoutes from './walletRoutes.js';
 import recordRoutes from './recordRoutes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/wallets', walletRoutes);
 router.use('/records', recordRoutes);
 
 export default router;
