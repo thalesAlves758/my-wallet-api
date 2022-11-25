1ef021807ce8896aa0bcb58c9a774fc7ad13b72e
Thales Alves <thalesalves758@gmail.com>
2022-11-25 15:45:34 -0300

    feat: create a function to update record by id

--- src/services/recordService.js
+++ src/services/recordService.js
@@ -1,13 +1,16 @@
 import { notFoundError } from '../errors/httpErrors.js';
-import create, { deleteById } from '../repositories/recordRepository.js';
-import { findById, updateBalance } from '../repositories/userRepository.js';
+import create, {
+  deleteById,
+  updateById,
+} from '../repositories/recordRepository.js';
+import { findById, incrementBalance } from '../repositories/userRepository.js';
 
 async function createRecord({ value, description, type, userId }) {
   await create({ value, description, type, userId });
 
   const valueToSum = type === 'input' ? value : toNegative(value);
 
-  await updateBalance(userId, valueToSum);
+  await incrementBalance(userId, valueToSum);
 
   const user = await findById(userId);
 
@@ -28,6 +31,24 @@ export async function deleteRecordById(recordId) {
   if (!result.value) {
     throw notFoundError('Could not find the specified record');
   }
+}
+
+export async function updateRecordById(recordId, { value, description }) {
+  const result = await updateById(recordId, { value, description });
+
+  if (!result.value) {
+    throw notFoundError('Could not find the specified record');
+  }
+}
+
+export function sumRecordsValues(records) {
+  const INITIAL_TOTAL = 0;
+
+  return records.reduce(
+    (totalValue, { value, type }) =>
+      type === 'input' ? totalValue + value : totalValue - value,
+    INITIAL_TOTAL
+  );
 }
 
 export default createRecord;
