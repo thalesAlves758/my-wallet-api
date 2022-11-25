b921dc19515e847d6221e638c55c239224096883
Thales Alves <thalesalves758@gmail.com>
2022-11-25 15:25:46 -0300

    feat: create a function to update a record by id

--- src/repositories/recordRepository.js
+++ src/repositories/recordRepository.js
@@ -15,4 +15,13 @@ export async function deleteById(recordId) {
   return db.collection('records').findOneAndDelete({ _id: ObjectId(recordId) });
 }
 
+export async function updateById(recordId, { value, description }) {
+  return db
+    .collection('records')
+    .findOneAndUpdate(
+      { _id: ObjectId(recordId) },
+      { $set: { value, description } }
+    );
+}
+
 export default create;
